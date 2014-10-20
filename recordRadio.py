8be3f5d2b2ba1464deef51df91f938ba139bed7e
Theodoros Giannakopoulos <tyiannak@gmail.com>
2014-10-21 00:36:16 +0300

    radio record

--- recordRadio.py
+++ recordRadio.py
@@ -1,13 +1,14 @@
 import sys, os
 stations = {}
 
-stations["skai"] = "http://159.253.149.12:8282"
+stations["skai"] = "http://netradio.live24.gr/skai1003"
+stations["somafm-sonicuniverse"] = "http://uwstream2.somafm.com:8604"
 
 def recordStation(stationName, outputName):
 #	command = "gst-launch-1.0 uridecodebin uri="+stations["skai"]+" ! tee name=t \ 
 #	t. ! queue ! audioresample ! audio/x-raw, rate=16000 ! wavenc ! filesink location=" + outputName
 	command = "gst-launch-1.0 uridecodebin uri=" + stations[stationName]+ " 	! tee name=t \ t. ! queue ! audioresample ! audio/x-raw, rate=16000 ! wavenc ! filesink location="+outputName
-	print command
+#	command = "gst-launch-1.0 souphttpsrc location=" + stations[stationName]+ " 	! tee name=t \ t. ! queue ! audioresample ! audio/x-raw, rate=16000 ! wavenc ! filesink location="+outputName
 	os.system(command)
 
 
