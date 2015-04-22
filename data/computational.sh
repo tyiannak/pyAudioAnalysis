192103ff16f95d8f6b27d7529bce3b5ec5dfaaab
Theodoros Giannakopoulos <tyiannak@gmail.com>
2015-04-23 01:30:39 +0300

    computational script

---
+++ data/computational.sh
@@ -0,0 +1,7 @@
+python testComputational.py -shortTerm
+python testComputational.py -classifyFile
+python testComputational.py -mtClassify
+python testComputational.py -hmmSegmentation
+python testComputational.py -silenceRemoval
+python testComputational.py -thumbnailing
+python testComputational.py -diarization
