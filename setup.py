5fcf382b0567404341f82a7b75d8c1ffcc7af158
Josef Hölzl <hoelzl.josef@gmail.com>
2016-10-19 09:24:44 +0200

    Adjusted setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,18 @@ try:
 except ImportError:
 	from distutils.core import setup
 
+try:
+	from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+	from distutils.command.build_py import build_py
+
 package = 'pyAudioAnalysis'
 version = '1.0'
 
 setup(name=package,
 	version=version,
-	packages = ["pyaudio_analysis"],
+	packages = ['pyaudio_analysis','data'],
+	cmdclass={'build_py': build_py},
 	author='Theodoros Giannakopoulos',
 	author_email='tyiannak@gmail.com',
 	long_description=open('README.md').read(),
