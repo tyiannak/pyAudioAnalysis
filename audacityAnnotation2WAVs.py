5b94422a2cffeed0849095777c14011fd4487bc7
Theodoros Giannakopoulos <tyiannak@gmail.com>
2017-03-04 01:37:44 +0200

    audacityAnnotation2WAVs.py extract series of annotated wav files based on audacity annotations and a respective mp3 or wav recording

--- audacityAnnotation2WAVs.py
+++ audacityAnnotation2WAVs.py
@@ -1,3 +1,4 @@
+import glob, os
 import audioBasicIO, sys, csv
 import scipy.io.wavfile as wavfile
 
@@ -26,17 +27,38 @@ def annotation2files(wavFile, csvFile):
             wavfile.write(label, Fs, xtemp)  
 
 def main(argv):
-    wavFile = argv[1]
-    annotationFile = argv[2]
-    annotation2files(wavFile, annotationFile)
+    if argv[1] == "-f":
+        wavFile = argv[2]
+        annotationFile = argv[3]
+        annotation2files(wavFile, annotationFile)
+    elif argv[1] == "-d":
+        inputFolder = argv[2]
+        types = ('*.txt', '*.csv')
+        annotationFilesList = []
+        for files in types:
+            annotationFilesList.extend(glob.glob(os.path.join(inputFolder, files)))
+        for anFile in annotationFilesList:
+            wavFile = os.path.splitext(anFile)[0] + ".wav"
+            if not os.path.isfile(wavFile):
+                wavFile = os.path.splitext(anFile)[0] + ".mp3"
+                if not os.path.isfile(wavFile):
+                    print "Audio file not found!"
+                    return
+            annotation2files(wavFile, anFile)
+
 
 if __name__ == '__main__':
-    # This file can be used to extract a series of annotated WAV files based on (a) an audio file (mp3 or wav) and 
+    # Used to extract a series of annotated WAV files based on (a) an audio file (mp3 or wav) and 
     # (b) a segment annotation file e.g. a "label" file generated in audacity
-    # usage:
-    # python audacityAnnotation2WAVs.py <audiofilepath> <annotationfilepath>
     #
+    # usage 1:
+    # python audacityAnnotation2WAVs.py -f <audiofilepath> <annotationfilepath>
     # The <annotationfilepath> is actually a tab-seperated file where each line has the format <startTime>\t<entTime>\t<classLabel>
     # The result of this process is a  series of WAV files with a file name <audiofilepath>_<startTime>_<endTime>_<classLabel>
+    # 
+    # usage 2:
+    # python audacityAnnotation2WAVs.py -d <annotationfolderpath>
+    # Same but searches all .txt and .csv annotation files. Audio files are supposed to be in the same path / filename with a WAV extension
+
     main(sys.argv)
     
