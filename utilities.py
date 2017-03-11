c23faf55a650d972a67e668a74b18c7b31f0c90f
majorbonghits420 <jcc384@cornell.edu>
2017-03-11 12:47:36 -0500

    tabs to spaces, removed extra whitespace

--- utilities.py
+++ utilities.py
@@ -1,48 +1,48 @@
 import sys, os, numpy
 
 def isfloat(x):
-	"""
-	Check if argument is float
-	"""
-	try:
-		a = float(x)
-	except ValueError:
-		return False
-	else:
-		return True
+        """
+        Check if argument is float
+        """
+        try:
+                a = float(x)
+        except ValueError:
+                return False
+        else:
+                return True
 
 def isint(x):
-	"""
-	Check if argument is int
-	"""
-	try:
-		a = float(x)
-		b = int(a)
-	except ValueError:
-		return False
-	else:
-		return a == b
+        """
+        Check if argument is int
+        """
+        try:
+                a = float(x)
+                b = int(a)
+        except ValueError:
+                return False
+        else:
+                return a == b
 
 def isNum(x):
-	"""
-	Check if string argument is numerical
-	"""
-	return isfloat(x) or isint(x)
+        """
+        Check if string argument is numerical
+        """
+        return isfloat(x) or isint(x)
 
 
 def peakdet(v, delta, x = None):
     """
     Converted from MATLAB script at http://billauer.co.il/peakdet.html
-    
+
     Returns two arrays
-    
+
     function [maxtab, mintab]=peakdet(v, delta, x)
     %PEAKDET Detect peaks in a vector
     %        [MAXTAB, MINTAB] = PEAKDET(V, DELTA) finds the local
     %        maxima and minima ("peaks") in the vector V.
     %        MAXTAB and MINTAB consists of two columns. Column 1
     %        contains indices in V, and column 2 the found values.
-    %      
+    %
     %        With [MAXTAB, MINTAB] = PEAKDET(V, DELTA, X) the indices
     %        in MAXTAB and MINTAB are replaced with the corresponding
     %        X-values.
@@ -50,33 +50,33 @@ def peakdet(v, delta, x = None):
     %        A point is considered a maximum peak if it has the maximal
     %        value, and was preceded (to the left) by a value lower by
     %        DELTA.
-    
+
     % Eli Billauer, 3.4.05
     % This function is released to the public domain; Any use is allowed.
-    
+
     """
     maxtab = []
     mintab = []
-       
+
     if x is None:
         x = numpy.arange(len(v))
-    
+
     v = numpy.asarray(v)
-    
+
     if len(v) != len(x):
         sys.exit('Input vectors v and x must have same length')
-    
+
     if not numpy.isscalar(delta):
         sys.exit('Input argument delta must be a scalar')
-    
+
     if delta <= 0:
         sys.exit('Input argument delta must be positive')
-    
+
     mn, mx = numpy.Inf, -numpy.Inf
     mnpos, mxpos = numpy.NaN, numpy.NaN
-    
+
     lookformax = True
-    
+
     for i in numpy.arange(len(v)):
         this = v[i]
         if this > mx:
@@ -85,7 +85,7 @@ def peakdet(v, delta, x = None):
         if this < mn:
             mn = this
             mnpos = x[i]
-        
+
         if lookformax:
             if this < mx-delta:
                 maxtab.append(mxpos)
@@ -98,6 +98,6 @@ def peakdet(v, delta, x = None):
                 mx = this
                 mxpos = x[i]
                 lookformax = True
- 
+
     return numpy.array(maxtab), numpy.array(mintab)
 
