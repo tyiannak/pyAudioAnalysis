53bb23da3ddf24acd93803f3f9e21d0dd9647639
tyiannak <tyiannak@gmail.com>
2018-09-04 01:50:19 +0300

    version update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyAudioAnalysis',
-      version='0.2.3',
+      version='0.2.5',
       description='Python Audio Analysis Library: Feature Extraction, Classification, Segmentation and Applications',
       url='https://github.com/tyiannak/pyAudioAnalysis',
       author='Theodoros Giannakopoulos',
