e56c56f6264ff003ad14fba8e99d29b225896fe5
Theodoros Giannakopoulos <tyiannak@gmail.com>
2020-02-12 00:15:11 +0200

    minor

--- tests/cmd_test_06.sh
+++ tests/cmd_test_06.sh
@@ -3,7 +3,7 @@ echo $data_root
 command1="python3 ../pyAudioAnalysis/audioAnalysis.py segmentationEvaluation --model svm --modelName \"${data_root}//models/svm_rbf_sm\"  -i \"${data_root}/radioFinal/test\""
 echo $command1
 eval $command1
-command2="python3 ../pyAudioAnalysis/audioAnalysis.py segmentationEvaluation --model knn --modelName \"${data_root}//models/knn_rbf_sm\"  -i \"${data_root}/radioFinal/test\""
+command2="python3 ../pyAudioAnalysis/audioAnalysis.py segmentationEvaluation --model knn --modelName \"${data_root}//models/knn_sm\"  -i \"${data_root}/radioFinal/test\""
 echo $command2
 eval $command2
 command3="python3 ../pyAudioAnalysis/audioAnalysis.py segmentationEvaluation --model hmm --modelName \"${data_root}//hmmRadioSM\"  -i \"${data_root}/radioFinal/test\""
