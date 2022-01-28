f27f2265579f6c56ab1f838f45556058906dde8a
Theodoros Giannakopoulos <tyiannak@gmail.com>
2022-01-29 01:45:37 +0200

    pytest mtfileclassifcation and training changes

--- pytests/test_segmentation.py
+++ pytests/test_segmentation.py
@@ -15,5 +15,10 @@ def test_speaker_diarization():
     assert purity_speaker_m > 0.9, "Diarization speaker purity is low"
 
 
-    #TODOs 
-    # 1) mid term classification (scottish using existing model in rep)
+def test_mt_file_classification():
+    labels, class_names, accuracy, cm = aS.mid_term_file_classification(
+                                     "test_data/scottish.wav", 
+                                     "test_data/svm_rbf_sm", "svm_rbf", False, 
+                                     "test_data/scottish.segments")
+    assert accuracy > 0.95, "Segment-level classification accuracy is low"
+    
